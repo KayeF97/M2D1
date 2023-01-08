fd29259c61c84fa0bfdc529234d6cf3698ee5d18
Kaye <kandyce.everowe@gmail.com>
2023-01-08 16:10:23 +0000

    carbonara recipe

---
+++ index.js
@@ -0,0 +1,16 @@
+//Spaghetti carbonara recipe
+
+
+//carbonara steps
+
+let cut = 250;
+let combine = 6 + 50;
+let roast = combine + 4;
+let brown = 400;
+let cook = cut;
+let sauce = brown;
+let mix = sauce + cook;
+let carbonara = mix + roast;
+
+console.log(carbonara); 
+
